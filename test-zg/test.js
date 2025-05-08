4a58c0b6d9dd61a8ac4c3ef416b6b6e10b428bf1
Gustavo Cardoso Costa <gustavocardoso0019@gmail.com>
2025-05-08 11:33:10 -0300

    Update test.js

--- test-zg/test.js
+++ test-zg/test.js
@@ -1,23 +1,23 @@
 require('dotenv').config()
 const axios = require('axios')
 
-// axios.get('https://desafio-endpoint-hashcode-n2.onrender.com/passo1')
-//     .then(response => {
-//         console.log('Response: ', response.data)
-//     })
-//     .catch(error => {
-//         conso.error('Error: ', error)
-//     })
+axios.get('https://desafio-endpoint-hashcode-n2.onrender.com/passo1')
+    .then(response => {
+        console.log('Response: ', response.data)
+    })
+    .catch(error => {
+        conso.error('Error: ', error)
+    })
 
-// axios.post('https://desafio-endpoint-hashcode-n2.onrender.com/passo2', {
-//         cpf: '114.803.415-33'
-//     })
-//     .then(function (response) {
-//         console.log('Response: ',response.data);
-//     })
-//     .catch(function (error) {
-//         console.log('Error: ',error.response?.data);
-//     });
+axios.post('https://desafio-endpoint-hashcode-n2.onrender.com/passo2', {
+        cpf: '114.803.415-33'
+    })
+    .then(function (response) {
+        console.log('Response: ',response.data);
+    })
+    .catch(function (error) {
+        console.log('Error: ',error.response?.data);
+    });
 
 const token = process.env.TOKEN_ACCESS
 
